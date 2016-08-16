0abfc88c90769fef7dc62d62b4f3e17c6aab6da3
BoyceLiu <boyceliu123@gmail.com>
2016-08-16 19:34:34 +0800

    second

--- volume 2/Chapter 4/测试getopt函数/main.c
+++ volume 2/Chapter 4/测试getopt函数/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char * const argv[]) {
 	int opt;
-	while ((opt=getopt(argc, argv, "ab:c::")) != -1) {
+	while ((opt=getopt(argc, argv, ":ab:c::")) != -1) {
 		switch (opt) {
 			case 'a' : printf("no argument\n"); break;
 			case 'b' : printf("one argument\n"); break;
